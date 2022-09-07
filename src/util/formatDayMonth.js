2b65a22fdba729f53130bd84c5fdff395decf545
Andrew Aghoghovwia <androyt1@gmail.com>
2022-09-07 07:31:23 +0100

    some final modifications

--- src/util/formatDayMonth.js
+++ src/util/formatDayMonth.js
@@ -1,6 +1,6 @@
 
 function formatDayMonth(date){
-    const d=new Date(`${date}`).toDateString()
+    const d=new Date(date).toDateString()
     const x=d.split(" ")
     return `${x[1]} ${x[2]}, ${x[3]}`
 }
